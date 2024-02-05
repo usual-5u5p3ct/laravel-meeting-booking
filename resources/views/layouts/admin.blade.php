8731646d933dc1085b973a1996ac6360380d194c
Thariq bin Iskandar <thariq@cpglobalinnovation.com>
2024-02-05 14:41:04 +0800

    added logo to navbar

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -34,8 +34,7 @@
             <span class="navbar-toggler-icon"></span>
         </button>
         <a class="navbar-brand" href="#">
-            <span class="navbar-brand-full">{{ trans('panel.site_title') }}</span>
-            <span class="navbar-brand-minimized">{{ trans('panel.site_title') }}</span>
+            <span class="navbar-brand-full"><img src="{{ asset('images/cpgi-logo.png') }}" alt="cpgi-logo" width="65" height="30"></span>
         </a>
         <button class="navbar-toggler sidebar-toggler d-md-down-none" type="button" data-toggle="sidebar-lg-show">
             <span class="navbar-toggler-icon"></span>
